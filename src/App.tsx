aea38c3b2c6f2e09e9212ba80620bb012cce6e85
RuushTea <RushdiRamz@gmail.com>
2025-06-12 05:09:16 +0530

    refactor: update App and Buttons components to improve alert handling and simplify button rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Buttons from "./components/Buttons";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Emergency Alert</Alert>
+      )}
       <Buttons
         children="Click me"
-        onClick={() => console.log("Clicked")}
+        onClick={() => setAlertVisibility(true)}
         color="danger"
       ></Buttons>
     </>
--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,12 +1,10 @@
 import { ReactNode, useState } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
+  children: string;
   color?: "primary" | "secondary" | "danger" | "success";
   onClick: () => void;
 }
-
-const buttonArray = ["Button 1", "Button 2", "Button 3"];
 
 function Buttons({ children, onClick, color = "primary" }: ButtonProps) {
   const [selectedButton, setSelectedButton] = useState("");
@@ -14,19 +12,9 @@ function Buttons({ children, onClick, color = "primary" }: ButtonProps) {
   return (
     <>
       <h1>Buttons</h1>
-      {buttonArray.map((button, index) => (
-        <button className={"btn btn-" + color} onClick={onClick}>
-          {button}
-        </button>
-      ))}
-      <div className="mt-3">
-        <button
-          className="btn btn-secondary"
-          onClick={() => console.log("Why did you click me..")}
-        >
-          {children}
-        </button>
-      </div>
+      <button className={"btn btn-" + color} onClick={onClick}>
+        {children}
+      </button>
     </>
   );
 }
